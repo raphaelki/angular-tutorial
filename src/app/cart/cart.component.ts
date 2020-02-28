bd8afdf8997be3ef8a0d33fe3c2ade62b4e11991
raphaelki <raphael.kirchner@yahoo.de>
2020-02-28 08:41:47 +0100

    Add items to cart and show overview

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from "@angular/core";
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
+  styleUrls: ["./cart.component.css"]
+})
+export class CartComponent implements OnInit {
+  cartItems;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.cartItems = this.cartService.getItems();
+  }
+}
